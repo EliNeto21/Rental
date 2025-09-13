8018b4402f965bf28574ca7e406eb702018a0e45
Eli Neto <elineto91@gmail.com>
2025-09-13 11:51:27 -0300

    #ADD Migrations

---
+++ Infra/Migrations/20250913023504_InitialCreate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "couriers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Cnpj = table.Column<string>(type: "text", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    CnhNumber = table.Column<string>(type: "text", nullable: false),
+                    CnhType = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    CnhImageUrl = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_couriers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "motorcycles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Plate = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_motorcycles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rentals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MotorcycleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CourierId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlanDays = table.Column<int>(type: "integer", nullable: false),
+                    DailyRate = table.Column<decimal>(type: "numeric", nullable: false),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    ExpectedEndDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rentals", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_couriers_CnhNumber",
+                table: "couriers",
+                column: "CnhNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_couriers_Cnpj",
+                table: "couriers",
+                column: "Cnpj",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_motorcycles_Plate",
+                table: "motorcycles",
+                column: "Plate",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "couriers");
+
+            migrationBuilder.DropTable(
+                name: "motorcycles");
+
+            migrationBuilder.DropTable(
+                name: "rentals");
+        }
+    }
+}
